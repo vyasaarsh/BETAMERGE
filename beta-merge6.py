889be9858fee9d482efbeb40b502101917d06f71
Parth <taksuparth@gmail.com>
2024-10-20 17:38:17 +0530

    chore: extract out utils

--- beta-merge6.py
+++ beta-merge6.py
@@ -1,121 +1,13 @@
 # Libraries
 import streamlit as st
 import pandas as pd 
-import paramiko
-import plotly.express as px
 from time import sleep
 from collections import defaultdict, deque
 
 from constants.timeRange import TimeRange
 from utils.getTimeRangeSpecificData import get_time_specific_data
-
-# Function to establish SSH connection
-def connect_ssh_agent(username): 
-    hostname = "rt1.olsendata.com"
-    port = 22103
-    password = "aar5hvya5"
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(hostname, port, username, password)
-    return ssh.invoke_shell()
-
-# Parse Real-Time (rt) data from SSH connection
-def parse_real_time_data(data, historic_data):
-    lines = data.strip().split('\n')
-    table_data = []
-    for line in lines:
-        if line.startswith('!'):
-            _, timestamp = line.split(',')
-        else:
-            try:
-                symbol, last_price, _ = line.split(',')
-            except ValueError:
-                continue
-            change = 0
-            if symbol in historic_data:
-                trend = historic_data[symbol]
-                change = float(last_price) - trend[-1]
-                trend.append(float(last_price))
-                if len(trend) > 100:
-                    trend.popleft()
-                historic_data[symbol] = trend
-            else: 
-                historic_data[symbol] = deque([float(last_price)])
-            table_data.append({
-                'Symbol': symbol,
-                'Price': float(last_price),
-                'Change': change,
-                '% Change': (change / float(last_price)) * 100,
-                'Trend': list(historic_data[symbol]),
-            })
-    return table_data
-
-# Parse Historical (hist1s) data from SSH connection
-def parse_hist1s_data(data):
-    lines = data.strip().split('\n')
-    parsed_data = []
-    date = None
-    for line in lines:
-        if line.startswith('!'):
-            parts = line.split(',')
-            if len(parts) >= 2:
-                date, timestamp = parts[:2]
-                parsed_data.append({'Type': 'timestamp', 'Date': date, 'Time': timestamp})
-            else:
-                continue
-        else:
-            try:
-                symbol, last_price, _ = line.split(',')
-                parsed_data.append({'Symbol': symbol, 'Last Price': float(last_price)})
-            except ValueError:
-                continue
-    return parsed_data
-
-# Parse Historical (hist1m) data from SSH connection
-def parse_hist1m_data(data):
-    lines = data.strip().split('\n')
-    parsed_data = []
-    date = None
-    for line in lines:
-        if line.startswith('!'):
-            parts = line.split(',')
-            if len(parts) == 2:
-                date, timestamp = parts
-                timestamp = f"{timestamp}:00"
-                parsed_data.append({'Type': 'timestamp', 'Date': date, 'Time': timestamp})
-            else:
-                continue
-        else:
-            try:
-                symbol, last_price, _ = line.split(',')
-                parsed_data.append({'Symbol': symbol, 'Last Price': float(last_price)})
-            except ValueError:
-                continue
-    return parsed_data
-
-# Parse Historical (hist1h) data from SSH connection
-def parse_hist1h_data(data):
-    lines = data.strip().split('\n')
-    parsed_data = []
-    current_symbol = None
-    
-    for line in lines:
-        if line.startswith('#'):
-            current_symbol = line[1:].strip()
-        else:
-            try:
-                date, timestamp, last_price, _ = line.split(',')
-                timestamp = f"{date} {timestamp}:00:00"
-                parsed_data.append({
-                    'Symbol': current_symbol,
-                    'Last Price': float(last_price),
-                    'Date': date,
-                    'Timestamp': timestamp
-                })
-            except ValueError:
-                continue
-    
-    return parsed_data
+from utils.connectionUtils import connect_ssh_agent
+from utils.dataParser import parse_real_time_data, parse_hist1h_data, parse_hist1m_data, parse_hist1s_data
 
 # Retrieve Real-Time (rt) data
 def get_real_time_data_rt(channel, historic_data):
@@ -166,7 +58,11 @@ def main():
     
     # Fetch historical data
     historical_data_1h = get_historical_data("hist1h")
+    # historical_data_1s = get_historical_data("hist1s")
+    # historical_data_1m = get_historical_data("hist1m")
     parsed_hist1h_data = parse_hist1h_data(historical_data_1h)
+    # parsed_hist1s_data = parse_hist1s_data(historical_data_1s)
+    # parsed_hist1m_data = parse_hist1m_data(historical_data_1m)
 
     st.title("Real Time and Historical Prices")
     st.divider()
@@ -190,25 +86,23 @@ def main():
     # Create a line chart for historical data
     st.subheader(f"Historical Data for {selected_time_range.value}")
     
-    if time_range_data:
-        df_time_range = pd.DataFrame(time_range_data)
+    # if time_range_data:
+    #     df_time_range = pd.DataFrame(time_range_data)
         
-        # Ensure that the dataframe contains 'Timestamp' and 'Last Price' for plotting
-        if 'Timestamp' in df_time_range.columns and 'Last Price' in df_time_range.columns:
-            fig = px.line(
-                df_time_range,
-                x='Timestamp',
-                y='Last Price',
-                title=f"{selected_time_range.value} Price Trends",
-                labels={'Timestamp': 'Date/Time', 'Last Price': 'Price'},
-                line_shape='linear',
-            )
-            fig.update_layout(xaxis_title='Date/Time', yaxis_title='Price', template='plotly_white')
-            st.plotly_chart(fig, use_container_width=True)
-        else:
-            st.warning("No data available for the selected time range.")
-
-
+    #     # Ensure that the dataframe contains 'Timestamp' and 'Last Price' for plotting
+    #     if 'Timestamp' in df_time_range.columns and 'Last Price' in df_time_range.columns:
+    #         fig = px.line(
+    #             df_time_range,
+    #             x='Timestamp',
+    #             y='Last Price',
+    #             title=f"{selected_time_range.value} Price Trends",
+    #             labels={'Timestamp': 'Date/Time', 'Last Price': 'Price'},
+    #             line_shape='linear',
+    #         )
+    #         fig.update_layout(xaxis_title='Date/Time', yaxis_title='Price', template='plotly_white')
+    #         st.plotly_chart(fig, use_container_width=True)
+    #     else:
+    #         st.warning("No data available for the selected time range.")
 
     while True:
         # Fetch and display real-time data
---
+++ utils/dataParser.py
@@ -0,0 +1,101 @@
+from collections import deque
+
+# Parse Real-Time (rt) data from SSH connection
+def parse_real_time_data(data, historic_data):
+    lines = data.strip().split('\n')
+    table_data = []
+    for line in lines:
+        if line.startswith('!'):
+            _, timestamp = line.split(',')
+        else:
+            try:
+                symbol, last_price, _ = line.split(',')
+            except ValueError:
+                continue
+            change = 0
+            if symbol in historic_data:
+                trend = historic_data[symbol]
+                change = float(last_price) - trend[-1]
+                trend.append(float(last_price))
+                if len(trend) > 100:
+                    trend.popleft()
+                historic_data[symbol] = trend
+            else: 
+                historic_data[symbol] = deque([float(last_price)])
+            table_data.append({
+                'Symbol': symbol,
+                'Price': float(last_price),
+                'Change': change,
+                '% Change': (change / float(last_price)) * 100,
+                'Trend': list(historic_data[symbol]),
+            })
+    return table_data
+
+# Parse Historical (hist1s) data from SSH connection
+def parse_hist1s_data(data):
+    lines = data.strip().split('\n')
+    parsed_data = []
+    date = None
+    for line in lines:
+        if line.startswith('!'):
+            parts = line.split(',')
+            if len(parts) >= 2:
+                date, timestamp = parts[:2]
+                parsed_data.append({'Type': 'timestamp', 'Date': date, 'Time': timestamp})
+            else:
+                continue
+        else:
+            try:
+                symbol, last_price, _ = line.split(',')
+                parsed_data.append({'Symbol': symbol, 'Last Price': float(last_price)})
+            except ValueError:
+                continue
+    return parsed_data
+
+# Parse Historical (hist1m) data from SSH connection
+def parse_hist1m_data(data):
+    lines = data.strip().split('\n')
+    parsed_data = []
+    date = None
+    for line in lines:
+        if line.startswith('!'):
+            parts = line.split(',')
+            if len(parts) == 2:
+                date, timestamp = parts
+                timestamp = f"{timestamp}:00"
+                parsed_data.append({'Type': 'timestamp', 'Date': date, 'Time': timestamp})
+            else:
+                continue
+        else:
+            try:
+                symbol, last_price, _ = line.split(',')
+                parsed_data.append({'Symbol': symbol, 'Last Price': float(last_price)})
+            except ValueError:
+                continue
+    return parsed_data
+
+# Parse Historical (hist1h) data from SSH connection
+def parse_hist1h_data(data):
+    lines = data.strip().split('\n')
+    parsed_data = []
+    current_symbol = None
+    
+    for line in lines:
+        if line.startswith('#'):
+            current_symbol = line[1:].strip()
+        else:
+            try:
+                date, timestamp, last_price, _ = line.split(',')
+                timestamp = f"{date} {timestamp}:00:00"
+                parsed_data.append({
+                    'Symbol': current_symbol,
+                    'Last Price': float(last_price),
+                    'Date': date,
+                    'Timestamp': timestamp
+                })
+            except ValueError:
+                continue
+    
+    return parsed_data
+
+__all__ = ['parse_hist1h_data', 'parse_hist1m_data', 'parse_hist1s_data', 'parse_real_time_data']
